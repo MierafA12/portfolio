2ae147d310be5974745de9238146b38cd01a3b82
MierafA12 <mierafabebe12@gmail.com>
2025-07-08 15:58:43 +0300

    initialize portfolio,set folder structure

---
+++ src/app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-blue-600 text-white text-3xl">
+      Hello Mieraf, Tailwind is working! 🎉
+    </main>
+  );
+}
