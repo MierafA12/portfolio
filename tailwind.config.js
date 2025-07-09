a7614390c614c07a84584848f2e304bc002a9efa
MierafA12 <mierafabebe12@gmail.com>
2025-07-09 14:28:05 +0300

    configertailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#e63946",
+        background: "#f8f9fa",
+      },
+    },
+  },
+  plugins: [],
+};
