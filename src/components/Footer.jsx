1601d56b226a25ca2454de72c54f93ea20f488fb
MierafA12 <mierafabebe12@gmail.com>
2025-07-13 15:41:34 +0300

    footer part

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="text-center py-6 border-t dark:border-gray-700 mt-10">
+      <p>&copy; {new Date().getFullYear()} Mieraf Abebe. All rights reserved.</p>
+    </footer>
+  );
+}
