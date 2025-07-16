a5ad2d92864f96c93f9822e0695747c61f9361b1
MierafA12 <mierafabebe12@gmail.com>
2025-07-16 22:05:31 +0300

    update

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,7 +1,7 @@
-
-
-export default function blog(){
-    return(
-        <h1>Coming_Soon</h1>
-    )
+export default function Blog() {
+  return (
+    <main className="h-screen flex items-center justify-center bg-[#f9f8fe] text-gray-800">
+      <h1 className="text-4xl font-bold text-[#C33235]">Coming Soon 🚧</h1>
+    </main>
+  );
 }
