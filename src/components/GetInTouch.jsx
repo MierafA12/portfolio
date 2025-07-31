be86eaf6b04256b7a52e5b9d5d3948869d48dd60
MierafA12 <mierafabebe12@gmail.com>
2025-07-31 16:00:56 +0300

    responsivness

--- src/components/GetInTouch.jsx
+++ src/components/GetInTouch.jsx
@@ -4,16 +4,16 @@ import think from '../../public/think.png';
 
 export default function GetInTouch() {
   return (
-    <section className=" px-6 md:px-20 bg-white">
-      <div className="flex flex-col md:flex-row items-center justify-between gap-16">
+    <section className="px-4 md:px-20 py-10 bg-white">
+      <div className="flex flex-row flex-wrap items-center justify-between gap-6">
         {/* Text Section */}
-        <div className="md:w-1/2 space-y-6">
-          <h1 className="text-3xl md:text-4xl font-bold text-gray-800">
+        <div className="flex-1 min-w-[250px] space-y-6">
+          <h1 className="text-2xl md:text-3xl font-bold text-gray-800">
             Is there any idea in your mind?
           </h1>
-          <p className="text-gray-600 text-lg max-w-xl">
+          <p className="text-gray-600 text-base md:text-lg max-w-xl">
             Let's talk and turn your idea into a real-world project — I'm here to help bring it to life.
-            </p>
+          </p>
           <Link
             href="/contacts"
             className="inline-block bg-[#C33235] text-white px-6 py-3 rounded-full font-medium hover:bg-red-700 transition duration-300"
@@ -23,11 +23,11 @@ export default function GetInTouch() {
         </div>
 
         {/* Image Section */}
-        <div className="md:w-1/3">
+        <div className="flex-1 min-w-[250px]">
           <Image
             src={think}
             alt="Thinking about a new project"
-            className="w-full h-auto rounded-xl "
+            className="w-full h-auto rounded-xl object-contain"
             priority
           />
         </div>
